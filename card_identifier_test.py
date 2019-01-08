da60adc6384531b848110b6cc31faf17eac73d9e
iggy12345 <aaronjencks@gmail.com>
2019-01-07 22:43:31 -0600

    Fixed bugs with card test

--- card_identifier_test.py
+++ card_identifier_test.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
 # pylint: disable=E1101
 
+print('Importing Libraries')
 import sys
 import time
 import numpy as np
@@ -13,14 +14,16 @@ from utils.models import label_map_util
 from utils import visualization_utils_color as vis_util
 from utils.models.model_util import TensorflowFaceDetector
 
+print('Setting up paths')
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = './card_identifier/model/current/frozen_inference_graph.pb'
+PATH_TO_CKPT = './card_identifier/model/current/frozen_graph/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = './card_identifier/labelmap.pbtxt'
 
 NUM_CLASSES = 2
 
+print('Loading label maps')
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
@@ -40,10 +43,16 @@ if __name__ == "__main__":
     tDetector = TensorflowFaceDetector(PATH_TO_CKPT)
 
     cap = cv2.VideoCapture(camID)
+    
+    if not cap.isOpened():
+        cap.open()
+    
     windowNotSet = True
+    print('Starting computation')
     while True:
         ret, image = cap.read()
         if ret == 0:
+            print('Reading the frame from the camera failed, exitting')
             break
 
         [h, w] = image.shape[:2]
