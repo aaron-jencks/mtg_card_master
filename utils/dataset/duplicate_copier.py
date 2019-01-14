1fd4992888532200368216b49c0f9acfae0f0881
iggy12345 <aaronjencks@gmail.com>
2019-01-14 16:51:51 -0600

    Copied over duplicate cards to 3ED

--- utils/dataset/duplicate_copier.py
+++ utils/dataset/duplicate_copier.py
@@ -2,6 +2,43 @@ from shutil import copyfile
 import os
 import fnmatch
 import argparse
+import re
+
+exp_set = re.compile(r'^[0-9a-zA-Z]+')
+def remove_ext_and_set(filename):
+	"""Removes the file extension and the magic set
+	number from the given filename"""
+
+	global exp_set
+
+	m = exp_set.search(filename)
+
+	if m:
+		return os.path.splitext( filename)[0][m.end():]
+	else:
+		return os.path.splitext(filename)[0]
+
+# The next two methods use this
+exp = re.compile(r"\d+$")
+
+def get_num_id(filename):
+	"""Returns the numeric id number from a card filename"""
+
+	global exp
+
+	m = exp.search(filename)
+
+	return m.group() if m else None
+
+def remove_num_id(filename):
+	"""Removes the numeric id number from a card filename"""
+
+	global exp
+
+	m = exp.search(filename)
+	if m:
+		filename = filename[:m.start()]
+	return filename
 
 if __name__ == "__main__":
 	# Documentations and arguments
@@ -14,20 +51,30 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	if os.path.isdir(args.source_dir) and os.path.isdir(args.target_dir):
+		sid_dict = {}
 		source_files = []
 		for file in fnmatch.filter(os.listdir(args.source_dir), args.origin_set + "*.xml"):
 			print('found source {}'.format(file))
-			# appends the filename, dropping the extension
-			source_files.append(os.path.splitext(file)[0][len(args.origin_set):])
+			file = remove_ext_and_set(file)
+			c_id = get_num_id(file)
+			# appends the filename, dropping the extension, saves the id for later
+			file = remove_num_id(file)
+			source_files.append(file)
+			sid_dict[file] = c_id
+
+		tid_dict = {}
 
 		for file in fnmatch.filter(fnmatch.filter(os.listdir(args.target_dir), args.target_set + "*.jpg"), args.pattern):
-			# Remove file extension and set name
-			file = os.path.splitext(file)[0][len(args.target_set):]
-			# Removes the unique identifier
-			
+			file = remove_ext_and_set(file)
+			c_id = get_num_id(file)
+			# Remove file extension and set name, saves the id for later
+			file = remove_num_id(file)
+			tid_dict[file] = c_id
+
 			print('Found target file {}'.format(file))
 
 			if file in source_files:
 				print('found a match for {}'.format(file))
-				copyfile((args.origin_set + file + ".xml"), (args.target_set + file + ".xml"))
+				copyfile(os.path.join(args.source_dir, (args.origin_set + file + sid_dict[file] + ".xml")), \
+					os.path.join(args.target_dir, (args.target_set + file + tid_dict[file] + ".xml")))
 
