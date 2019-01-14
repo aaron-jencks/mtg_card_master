8476ca2d6cf5393cbacb25f086515715c9a7ea6f
iggy12345 <aaronjencks@gmail.com>
2019-01-13 23:38:49 -0600

    Started 2ED

--- utils/dataset/annotation_propagation_util.py
+++ utils/dataset/annotation_propagation_util.py
@@ -21,18 +21,21 @@ parser.add_argument("pattern", help="The fnmatch pattern to use", type=str)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-	print('Finding matches")
+	print('Finding matches')
 	files = []
-	for f in os.listdir(args.pattern_dir):
-		if fnmatch.fnmatch(f, args.pattern):
-			print('Located copying target ' + f)
-			files.append(f)
+	for f in fnmatch.filter(os.listdir(args.pattern_dir), args.pattern):
+		print('Located copying target ' + f)
+		files.append(f)
 	print('Copying files')
 	# Extracts the extension from the filename
-	_, ext = os.path.splitext(target_file)
-	output_path, _ = os.path.split(target_file)
+	_, ext = os.path.splitext(args.target_file)
+	output_path, _ = os.path.split(args.target_file)
 	for f in files:
 		name, _ = os.path.splitext(os.path.basename(f))
 		new_file = os.path.join(output_path, name + ext)
 		print('Copying to ' + new_file)
-		copyfile(args.target_file, new_file)
+		try:
+			copyfile(args.target_file, new_file)
+		except Exception as e:
+			print(e)
+			continue
